b166e21efdb96d9f1c6074e76cc5ceecc6944877
WvdMeijs <wijnand.van.der.meijs@virtualsciences.nl>
2023-04-15 16:32:59 +0200

    did not improve things

--- web-scraper/login.py
+++ web-scraper/login.py
@@ -5,7 +5,7 @@ import envfile
 login_url = envfile.LOGIN_URL
 url=envfile.URL
 email = envfile.EMAIL
-password = "potatta"#envfile.PASSWORD
+password = envfile.PASSWORD
 
 # Data to be sent in the request body
 data = {
@@ -13,19 +13,13 @@ data = {
     'ctl00$cphMain$txtLgnPassw': password
 }
 
-def login_to_site():
-    # Send POST request to the login form
-    response = requests.options(login_url, data=data)
+# Send POST request to the login form
+response = requests.options(login_url, data=data)
+html = response.text
+print(html)
+# Check the response status code
+if response.status_code == 200:
+    print('Login successful babyyyyyy!')
+else:
+    print('Login failed :(')
 
-    # Check the response status code
-    if response.status_code == 200:
-        print('Login successful babyyyyyy!')
-    else:
-        print('Login failed :(')
-    # # Check if the login was successful
-    # if '<div id="popup_message">Inloggen mislukt!</div>' in response.content.decode():
-    #     print('Login failed')
-    # else:
-    #     print('Login successful!')
-
-login_to_site()
